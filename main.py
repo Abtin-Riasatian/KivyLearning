767fb99de991cdd49ab8b2b9d22223b2839c8809
Abtin Riasatian <abtinriasatian@gmail.com>
2016-09-23 14:54:08 +0330

    Part7-TouchInput

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from kivy.app import App
 # from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget  import Widget
+
+class TouchInput(Widget):
+    def on_touch_down(self, touch):
+        print("touch down" , touch)
+
+    def on_touch_up(self, touch):
+        print("touch up" , touch)
+
+    def on_touch_move(self, touch):
+        print ("touch move" , touch)
 
 class FirstKivy(App):
     def build(self):
-        return FloatLayout()
+        return TouchInput()
 
 
 if __name__ == "__main__":
